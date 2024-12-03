4cbb09946732beb6b401b7288ff542aaf55e016a
Sulthan Farras Nata Saputra <if23.sulthansaputra@mhs.ubpkarawang.ac.id>
2024-12-03 14:42:45 +0700

    Delete header.php

--- header.php
+++
@@ -1,5 +0,0 @@
-<header class="header-container text-center">
-    <h1 class="display-4 text-uppercase fw-bold">Selamat Datang di S2R Rental</h1>
-    <p class="lead mt-3 text-muted"><strong>Penyewaan Motor Terpercaya dan Terjangkau</strong></p>
-    <a href="#vehicles" class="btn btn-gradient-blue mt-4 px-4 py-2 fw-bold text-white shadow">Lihat Kendaraan Kami</a>
-</header>
